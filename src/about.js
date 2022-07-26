b7c3272e21ab7effbc2b462fe6ddfb3e127fc52b
bwitek-dev <kwanderleute@gmail.com>
2022-07-26 21:30:30 +0200

    Improve design of 'about' tab

--- src/about.js
+++ src/about.js
@@ -1,6 +1,6 @@
 import furnaceUrl from './images/furnace.jpg';
 
-function renderAboutTab(){
+function renderAboutTab(pageDescription){
     const img = document.createElement(`img`);
     img.setAttribute(`src`, furnaceUrl);
     img.setAttribute(`alt`, `pic of our furnace`);
@@ -8,6 +8,9 @@ function renderAboutTab(){
     const imgWrapper = document.createElement(`div`);
     imgWrapper.classList.add(`img-wrapper`);
     imgWrapper.appendChild(img);
+
+    const h2 = document.createElement(`h2`);
+    h2.textContent = `About us`;
 
     const p = document.createElement(`p`);
     p.setAttribute(`id`, `restaurant-description`);
@@ -19,11 +22,16 @@ function renderAboutTab(){
     bonbon. Chocolate bar marshmallow gummies sugar plum candy canes powder chupa chups shortbread. Carrot cake chupa chups
     tootsie roll toffee cake. Lollipop ice cream marshmallow brownie gummi bears soufflé oat cake jelly. Jelly-o danish
     dragée liquorice powder oat cake.`
+    const description = document.createElement(`div`);
+    description.setAttribute(`id`,`about-description`);
+
+    description.appendChild(h2)
+    description.appendChild(p)
 
     const aboutTab = document.createElement(`div`);
     aboutTab.setAttribute(`id`,`about-tab`);
     aboutTab.appendChild(imgWrapper)
-    aboutTab.appendChild(p);
+    aboutTab.appendChild(description);
 
     const selectedContent = document.querySelector(`#selected-content`);
     selectedContent.appendChild(aboutTab)
